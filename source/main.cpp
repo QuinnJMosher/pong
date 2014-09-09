dc53c74cc8ef574f221ad9907e987c9dca45f653
QuinnJMosher <Darkstaro59@gmail.com>
2014-09-09 15:40:14 -0700

    starting file

---
+++ source/main.cpp
@@ -0,0 +1,22 @@
+﻿#include "AIE.h"
+#include <iostream>
+
+int main( int argc, char* argv[] )
+{	
+	//constant vars
+	static int SCREEN_MAX_X = 1200, SCREEN_MAX_Y = 700;
+
+	Initialise(SCREEN_MAX_X, SCREEN_MAX_Y, false, "My Awesome Game");
+
+    //Game Loop
+    do
+	{
+
+        ClearScreen();
+
+    } while(!FrameworkUpdate());
+
+    Shutdown();
+
+    return 0;
+}
